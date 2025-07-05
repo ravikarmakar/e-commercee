6867c932a77b26ad708af1829a7ea47993b6c197
Ravi Karmakar <ravikarmkar94475@gmail.com>
2025-07-05 16:17:20 +0530

    working on checkout and apply coupon code features

---
+++ client/src/app/checkout/checkoutContent.tsx
@@ -0,0 +1,263 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { PayPalButtons } from "@paypal/react-paypal-js";
+
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Separator } from "@/components/ui/separator";
+
+import { useAddressStore } from "@/store/useAddressStore";
+import { useProductStore } from "@/store/useProductStore";
+import { CartItem, useCartStore } from "@/store/useCartStore";
+import { Coupon, useCouponStore } from "@/store/useCouponStore";
+
+const CheckoutContent = () => {
+  const router = useRouter();
+
+  const { addresses, fetchAddresses } = useAddressStore();
+  const { getProductById } = useProductStore();
+  const { items, fetchCart } = useCartStore();
+  const { fetchAllCoupons, couponList } = useCouponStore();
+
+  const [selectedAddress, setSelectedAddress] = useState<string | undefined>();
+  const [showPaymentFlow, setShowPaymentFlow] = useState(false);
+  const [checkoutEmail, setCheckoutEmail] = useState("");
+  const [cartItemsWithDetails, setCartItemsWithDetails] = useState<
+    (CartItem & { product: any })[]
+  >([]);
+  const [couponCode, setCouponCode] = useState("");
+  const [appliedCoupon, setAppliedCoupon] = useState<Coupon | null>(null);
+  const [couponAppliedError, setCouponAppliedError] = useState("");
+
+  useEffect(() => {
+    fetchAllCoupons();
+    fetchAddresses();
+    fetchCart();
+  }, [fetchAddresses, fetchCart, fetchAllCoupons]);
+
+  useEffect(() => {
+    const findDefaultAddress = addresses.find((address) => address.isDefault);
+    if (findDefaultAddress) {
+      setSelectedAddress(findDefaultAddress.id);
+    }
+  }, [addresses]);
+
+  useEffect(() => {
+    const fetchProductDetails = async () => {
+      const itemsWithDetails = await Promise.all(
+        items.map(async (item) => {
+          const product = await getProductById(item.productId);
+          return { ...item, product };
+        })
+      );
+      setCartItemsWithDetails(itemsWithDetails);
+    };
+
+    fetchProductDetails();
+  }, [items, getProductById]);
+
+  const handleApplyCoupon = () => {
+    const getCurrentCoupon = couponList.find(
+      (coupon) => coupon.code === couponCode.trim()
+    );
+
+    if (!getCurrentCoupon) {
+      setCouponAppliedError("Invalid coupon code");
+      setAppliedCoupon(null);
+      return;
+    }
+
+    const now = new Date();
+    if (
+      now < new Date(getCurrentCoupon.startDate) ||
+      now > new Date(getCurrentCoupon.endDate)
+    ) {
+      setCouponAppliedError("Coupon has expired");
+      setAppliedCoupon(null);
+      return;
+    }
+
+    if (getCurrentCoupon.usageCount >= getCurrentCoupon.usageLimit) {
+      setCouponAppliedError("Coupon usage limit reached");
+      setAppliedCoupon(null);
+      return;
+    }
+
+    setAppliedCoupon(getCurrentCoupon);
+    setCouponAppliedError("");
+    setCouponCode("");
+  };
+
+  const subTotal = cartItemsWithDetails.reduce(
+    (total, item) => total + (item.product.price || 0) * item.quantity,
+    0
+  );
+  const discountAmount = appliedCoupon
+    ? (subTotal * appliedCoupon.discountPercent) / 100
+    : 0;
+  const total = subTotal - discountAmount;
+
+  return (
+    <div className="min-h-screen bg-white py-8">
+      <div className="container mx-auto px-4 max--w-7xl">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <div className="lg:col-span-2 space-y-6">
+            <Card className="p-6">
+              <h2 className="text-xl font-semibold mb-4">Delivery</h2>
+              <div className="space-y-4">
+                {addresses.map((address) => (
+                  <div key={address.id} className="flex items-start space-x-2">
+                    <Checkbox
+                      id={address.id}
+                      checked={selectedAddress === address.id}
+                      onCheckedChange={() => setSelectedAddress(address.id)}
+                    />
+                    <Label htmlFor={address.id} className="flex-grow ml-3">
+                      <div>
+                        <span className="font-medium">{address.name}</span>
+                        {address.isDefault && (
+                          <span className="ml-2 text-sm text-green-600">
+                            (Default)
+                          </span>
+                        )}
+                      </div>
+                      <div className="text-sm text-gray-700">
+                        {address.address}
+                      </div>
+                      <div className="text-sm text-gray-600">
+                        {address.city}, {address.country}, {address.postalCode}
+                      </div>
+                      <div className="text-sm text-gray-600">
+                        {address.phone}
+                      </div>
+                    </Label>
+                  </div>
+                ))}
+                <Button onClick={() => router.push("/account")}>
+                  Add a new Address
+                </Button>
+              </div>
+            </Card>
+            <Card className="p-6">
+              {showPaymentFlow ? (
+                <div>
+                  <h3 className="text-xl font-semibold mb-4">Payment</h3>
+                  <p className="mb-3">
+                    All transactions are secure and encrypted
+                  </p>
+                  <PayPalButtons
+                    style={{
+                      layout: "vertical",
+                      color: "black",
+                      shape: "rect",
+                      label: "pay",
+                    }}
+                    fundingSource="card"
+                  />
+                </div>
+              ) : (
+                <div>
+                  <h3 className="text-xl font-semibold mb-4">
+                    Enter Email to get started
+                  </h3>
+                  <div className="gap-2 flex items-center">
+                    <Input
+                      type="email"
+                      placeholder="Enter your email"
+                      className="w-full"
+                      value={checkoutEmail}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setCheckoutEmail(e.target.value)
+                      }
+                    />
+                    <Button>Proceed to Buy</Button>
+                  </div>
+                </div>
+              )}
+            </Card>
+          </div>
+
+          {/* Order Summary */}
+          <div className="lg:col-span-1">
+            <Card className="p-6 sticky top-8">
+              <h2>Order summary</h2>
+              <div className="space-y-4">
+                {cartItemsWithDetails.map((item) => (
+                  <div key={item.id} className="flex items-center space-x-4">
+                    <div className="relative h-2- w-20 rounded-md overflow-hidden">
+                      <img
+                        src={item?.product?.images[0]}
+                        alt={item?.product?.name}
+                        className="object-cover"
+                      />
+                    </div>
+                    <div className="flex-1">
+                      <h3 className="font-medium">{item?.product?.name}</h3>
+                      <p className="text-sm text-gray-600">
+                        {item.color} / {item.size}
+                      </p>
+                      <p className="text-sm text-gray-500">
+                        Qty: {item.quantity}
+                      </p>
+                    </div>
+                    <p className="font-medium">
+                      ${(item?.product?.price * item.quantity).toFixed(2)}
+                    </p>
+                  </div>
+                ))}
+                <Separator />
+                <div className="space-y-2">
+                  <Input
+                    placeholder="Enter a Discount code or Gift code"
+                    onChange={(e) => setCouponCode(e.target.value)}
+                    value={couponCode}
+                  />
+                  <Button
+                    onClick={handleApplyCoupon}
+                    className="w-full"
+                    variant="outline"
+                  >
+                    Apply
+                  </Button>
+                  {couponAppliedError && (
+                    <p className="text-sm text-red-600">{couponAppliedError}</p>
+                  )}
+                  {appliedCoupon && (
+                    <p className="text-sm text-green-600">
+                      Coupon Applied Successfully!
+                    </p>
+                  )}
+                </div>
+                <Separator />
+                <div className="space-y-2">
+                  <div className="flex justify-between">
+                    <span>Subtotal</span>
+                    <span>${subTotal.toFixed(2)}</span>
+                  </div>
+                  {appliedCoupon && (
+                    <div className="flex justify-between text-green-500">
+                      <span>Discount ({appliedCoupon.discountPercent})%</span>
+                      <span>${discountAmount.toFixed(2)}</span>
+                    </div>
+                  )}
+                </div>
+                <Separator />
+                <div className="flex justify-between font-medium">
+                  <span>Total</span>
+                  <span>${total.toFixed(2)}</span>
+                </div>
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutContent;
--- client/src/app/checkout/page.tsx
+++ client/src/app/checkout/page.tsx
@@ -1,7 +1,18 @@
 "use client";
 
-const CheckoutPage = () => {
-  return <div>CheckoutPage</div>;
-};
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import CheckoutSuspense from "./checkoutSkeleton";
+
+function CheckoutPage() {
+  const options = {
+    clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!,
+  };
+
+  return (
+    <PayPalScriptProvider options={options}>
+      <CheckoutSuspense />
+    </PayPalScriptProvider>
+  );
+}
 
 export default CheckoutPage;
